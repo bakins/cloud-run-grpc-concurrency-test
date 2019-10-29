c341e48d3e17d5ab40256d014a1043a7cea2851e
Brian Akins <brian@akins.org>
2019-10-29 12:01:09 -0400

    hack

--- client/main.go
+++ client/main.go
@@ -20,16 +20,18 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"crypto/tls"
 	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
-	"net"
+	"net/http"
 	"net/url"
+	"strconv"
 	"sync"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
 )
 
@@ -54,30 +56,39 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to parse url: %v", err)
 	}
+	/*
+		var options []grpc.DialOption
 
-	var options []grpc.DialOption
-
-	if u.Scheme == "https" {
-		options = append(options, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})))
-	} else {
-		options = append(options, grpc.WithInsecure())
-	}
-
-	port := u.Port()
-	if port == "" {
 		if u.Scheme == "https" {
-			port = "443"
+			options = append(options, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})))
 		} else {
-			port = "80"
+			options = append(options, grpc.WithInsecure())
 		}
+
+
+		port := u.Port()
+		if port == "" {
+			if u.Scheme == "https" {
+				port = "443"
+			} else {
+				port = "80"
+			}
+		}
+
+		addr := net.JoinHostPort(u.Hostname(), port)
+		conn, err := grpc.Dial(addr, options...)
+		if err != nil {
+			log.Fatalf("did not connect: %v", err)
+		}
+		defer conn.Close()
+		c := pb.NewGreeterClient(conn)
+
+	*/
+
+	c := &client{
+		target: u.String(),
+		Client: http.Client{},
 	}
-	addr := net.JoinHostPort(u.Hostname(), port)
-	conn, err := grpc.Dial(addr, options...)
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
 
 	var wg sync.WaitGroup
 	for i := 0; i < workers; i++ {
@@ -99,4 +110,75 @@ func main() {
 		}()
 	}
 	wg.Wait()
+}
+
+type client struct {
+	target string
+	http.Client
+}
+
+func (c *client) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	var buf bytes.Buffer
+	if err := Write(&buf, req, false); err != nil {
+		return nil, err
+	}
+
+	//fmt.Println(c.target)
+
+	// path is /<package>.<service>/<method>
+	r, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodPost,
+		c.target+"/helloworld.Greeter/SayHello",
+		bytes.NewBuffer(buf.Bytes()),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	r.Header.Set("Content-Type", "application/grpc+proto")
+
+	resp, err := c.Client.Do(r)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			body = []byte("unknown")
+		}
+		return nil, fmt.Errorf("unexpected status: %d %s %s", resp.StatusCode, resp.Status, string(body))
+	}
+
+	var helloResponse pb.HelloReply
+
+	if err := Read(resp.Body, &helloResponse); err != nil {
+		fmt.Println(resp.Header)
+		return nil, fmt.Errorf("failed to read response: %v", err)
+	}
+
+	// must read until EOF to ensure trailers are read.
+	// there should be no data left before the trailers.
+	if _, err = resp.Body.Read([]byte{}); err != io.EOF {
+		return nil, fmt.Errorf("unexpected error: %v", err)
+	}
+
+	status := 0
+	grpcStatus := resp.Trailer.Get("Grpc-Status")
+	if grpcStatus != "" {
+		s, err := strconv.Atoi(grpcStatus)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse grpc-status %s: %v", grpcStatus, err)
+		}
+		status = s
+	}
+
+	if status != 0 {
+		return nil, fmt.Errorf("unexpected grpc status %d %s", status, resp.Trailer.Get("Grpc-Message"))
+	}
+
+	return &helloResponse, nil
 }
--- server/main.go
+++ server/main.go
@@ -32,10 +32,12 @@ import (
 	"sync/atomic"
 	"time"
 
+	"go.uber.org/zap"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -74,7 +76,12 @@ func main() {
 
 	//l := &listener{lis}
 
-	s := grpc.NewServer()
+	logger, _ := zap.NewProduction()
+	defer logger.Sync() // flushes buffer, if any
+
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_zap.UnaryServerInterceptor(logger)),
+	)
 
 	http.Handle("/helloworld.Greeter/", s)
 	pb.RegisterGreeterServer(s, &server{})
